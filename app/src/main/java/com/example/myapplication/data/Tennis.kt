03b48e946a2f3fa5b2184a32d2f65ae97ef5ec4a
OSIELJ <osieljuniormartins@gmail.com>
2024-10-17 08:16:39 -0300

    refactor: alterar nome da classe Tenis para Tennis e as sua variaveis em português para as respectivas em inglês.

---
+++ app/src/main/java/com/example/myapplication/data/Tennis.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.data
+
+data class Tennis(
+    val color: String,
+    val description: String,
+    val id: Int,
+    val imageUrl: String,
+    val mark: String,
+    val name: String
+)
